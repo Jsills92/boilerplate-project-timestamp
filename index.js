cb44a6c77b7708e095a48d74e958309092396080
Jake Sills <jsills2592@gmail.com>
2025-03-12 10:53:33 -0400

    Added the api route.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
+const { database } = require('pg/lib/defaults');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -22,6 +23,38 @@ app.get("/", function (req, res) {
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
+});
+
+app.get("/api/:date?", (req, res) => {
+  let dateParam = req.params.date;
+
+  //return current date if no date is provided
+  if(!dateParam) {
+    let currentDate = new Date();
+    return res.json({
+      unix: currentDate.getTime(),
+      utc: currentDate.toUTCString()
+    });
+  };
+
+  let date;
+
+  if(!isNaN(dateParam)) {
+    date = new Date(parseInt(dateParam)); //convert timestamp to Date object
+  } else {
+    date = new Date(dateParam); //try to convert string date
+  };
+
+  if(date.toString() === "Invalid Date") {
+    return res.json({ error: "Invalid Date" });
+  };
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+
+
 });
 
 
